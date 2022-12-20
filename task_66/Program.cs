df928ffb4b631a91d71214fa37be0feab3496847
rena <rena2004.rr@gmail.com>
2022-12-20 15:53:52 +0400

    homework9

---
+++ task_66/Program.cs
@@ -0,0 +1,15 @@
+﻿
+int Sum(int m, int n)
+{
+    if (m == n)
+        return n;
+    return n + Sum(m, n - 1);
+}
+
+
+Console.Clear();
+Console.Write("Введите число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сумма элементов от {m} до {n} = {Sum(m, n)}");;
